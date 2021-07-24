3d456f7d095878ce15a669d7634a54fbdcb2e3f9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-07-24 14:30:34 -0300

    modal js finished

---
+++ modal.js
@@ -0,0 +1,16 @@
+const Modal = {
+     open(){                    //Abrir modal / Ative ao modal/
+        document.querySelector('.modal-overlay')
+        .classList.add('active')
+    },
+    close(){                    // Fechar modal, remover a class active moddal/ 
+        document.querySelector('.modal-overlay')
+        .classList.remove('active')
+    }
+}
+
+const Transaction = {
+    incomes(){
+        
+    }
+}
