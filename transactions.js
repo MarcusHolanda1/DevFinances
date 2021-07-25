db438c690f6462364719c46c888ffc4383e5f35f
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-07-24 21:33:27 -0300

    calc + remove

--- transactions.js
+++ transactions.js
@@ -24,6 +24,19 @@ const transactions = [
 
 
 const Utils = {
+    formatAmount(value){
+        value = Number(value) * 100
+
+
+        return value;
+    },
+
+    formatDate(date){
+        const splittedDate = date.split('-')
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+        
+    }, 
+
     formatCurrency(value) {
         const signal = Number(value) < 0 ? '$' : ''
 
@@ -97,11 +110,12 @@ const DOM = {
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index 
 
         DOM.transactionsContainer.appendChild(tr)
     },
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -111,7 +125,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="dates">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
         `
         return html
@@ -133,16 +147,81 @@ const DOM = {
     }
 }
 
-/** Funnção para retornar o array */
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
 
+    getValues(){
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+    validateFields(){       // Verificar se todas as informações foram preenchidas
+        const {description, amount, date} = Form.getValues()
+        
+        if (description.trim() === "" || amount.trim() === "" || date.trim() === ""){
+            throw new Error('Por favor preencha todos os campos')
+        }
+    },
+
+    formatValues(){
+        let {description, amount, date} = Form.getValues()
+        
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+        
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    clearFields(){
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
+
+    submit(event) {
+        event.preventDefault()
+
+        try { 
+        Form.validateFields()
+       // formatar os dados para salvar 
+       const transaction = Form.formatValues()
+        // salvar 
+        Transaction.add(transaction)
+
+        // apagar os dados do formullário
+        Form.clearFields()
+         // modal feche
+        Modal.close()
+            
+        } catch (error) {
+            alert(error.message)            
+        }
+
+        
+
+
+        
+
+    }
+}
 
 /* APlioca;ão */
 
 const App = {
     init() {
 
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index)
         })
         
         DOM.updateBalance()
